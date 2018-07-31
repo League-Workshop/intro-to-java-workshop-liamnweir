685f7276dec9d3238fdfccb6f223a792747b3463
Colin Weir <Colin Weir@192.168.0.30>
2018-07-31 12:30:10 -0700

    day6

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -12,7 +12,7 @@ public class QuizGame {
 		String answer = JOptionPane.showInputDialog("Guess this farm animal: has a snout and rolls aroundd in the mud");
 		// 3. Use an if statement to check if their answer is correct
 		if (answer.equalsIgnoreCase("Pig")) {
-			JOptionPane.showInputDialog("You got it right! You earned 1 point");
+			JOptionPane.showInputDialog("You got it right! You earned 1 point. How do you feel?");
 		}
 
 		else {
@@ -25,7 +25,7 @@ public class QuizGame {
 
 		// 5. Create more questions by repeating steps 1, 2, and 3 below.
 		String goat = JOptionPane
-				.showInputDialog("Guess this farm animal: makes milk and some are hairy, some are shaved");
+				.showInputDialog("Guess this farm animal: makes milk and some are hairy, some are shaved. Also, they will use their horns to fight");
 
 		if (goat.equalsIgnoreCase("Goat")) {
 			JOptionPane.showInputDialog("You got it right! You earned 1 point");
@@ -47,11 +47,23 @@ public class QuizGame {
 		}
 
 		else {
-			JOptionPane.showInputDialog("Wrong! Better luck next question!");
+			JOptionPane.showMessageDialog(null,("Wrong! Better luck next question!"));
 		
 		}
+		String Sheep = JOptionPane.showInputDialog("Guess this farm animal: they are quiet hairy, and dogs are known to heard them up.");
+		if (Sheep.equalsIgnoreCase(Sheep)); {
+			JOptionPane.showMessageDialog(null,("Great job!"));
+		}
+		String chicken = JOptionPane.showInputDialog("Guess this farm animal: has wings, very yummy, and humans eat their babies!");
+		if (chicken.equalsIgnoreCase(chicken)); {
+			JOptionPane.showMessageDialog(null, ("You got it right!"));
+			 
+			
+			}
+		}
 	}
-}
+
+
 
 
 
